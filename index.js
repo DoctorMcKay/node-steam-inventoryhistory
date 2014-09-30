8611708c1358b1228595e7c0d1e9ee8b77e56589
Alexander Corn <mckay@doctormckay.com>
2014-09-30 00:12:09 -0400

    Add initial data

---
+++ index.js
@@ -0,0 +1,42 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+module.exports = InventoryHistory;
+
+function InventoryHistory() {
+	this._jar = request.jar();
+	this._request = request.defaults({"jar": this._jar});
+}
+
+InventoryHistory.prototype.setCookies = function(cookies) {
+	var self = this;
+	cookies.forEach(function(cookie) {
+		self.setCookie(cookie);
+	});
+};
+
+InventoryHistory.prototype.setCookie = function(cookie) {
+	this._jar.setCookieSync(request.cookie(cookie), 'http://steamcommunity.com');
+};
+
+InventoryHistory.prototype.getHistory = function(page, callback) {
+	if(typeof page === 'function') {
+		callback = page;
+		page = 1;
+	}
+	
+	console.log("requesting history");
+	this._request("http://steamcommunity.com/my/inventoryhistory?l=english&p=" + page, function(err, response, body) {
+		if(err) {
+			callback(err);
+			return;
+		}
+		
+		var output = {};
+		
+		var $ = cheerio.load(body);
+		output.totalTrades = parseInt($('.inventory_history_pagingrow').html().match(/\d+ - \d+ of (\d+) History Items/)[1]);
+		
+		callback(null, output);
+	});
+};
